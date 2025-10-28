4618e37af08c2ad6d316f2d65fb725e63fc524bb
engsoft163 - Julio Cesar de Ávila <engsoft163@students.ic.unicamp.br>
2025-10-28 16:25:33 -0300

    Ajuste no caminho do repositório

--- view/vite.config.ts
+++ view/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: "/unicamp-petshop-startup/",
   plugins: [react()],
   resolve: {
     alias: {
