190de25b87fa16380bb89b49b1342011aee4dad9
Julio Ávila <julio.avila@mc1global.com>
2025-11-03 21:15:25 -0300

    Outro ajuste na API para acessar o banco de dados

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -8,10 +8,10 @@ const dbConfig = {
   database: process.env.DB_NAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   port: process.env.DB_PORT || 5432,
-  ssl: process.env.DB_SSLMODE === 'true',
+  ssl: process.env.DB_SSLMODE === 'true' ? { rejectUnauthorized: false } : false,
   max: 20,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
 };
 
 const pool = new Pool(dbConfig);
